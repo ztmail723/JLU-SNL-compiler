cce9413da63f32a6b18f572feb3e1a9e21c7c2d9
ztmail723 <ztmail723@gmail.com>
2021-06-02 03:53:14 +0800

    添加了词法分析的单词类型

--- token.h
+++ token.h
@@ -1,11 +1,38 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#include <QString>
+
+enum class LexType
+{
+    /* 簿记单词符号 */
+    ENDFILE, ERROR,
+    /* 保留字 */
+    PROGRAM, PROCEDURE, TYPE, VAR, IF,
+    THEN, ELSE, FI, WHILE, DO, ENDWH,
+    BEGIN, END1, READ, WRITE, ARRAY, OF,
+    RECORD, RETURN1,
+    /* 类型 */
+    INTEGER, CHAR1,
+    /* 多字符单词符号 */
+    ID, INTC, CHARC,
+    /* 特殊符号 */
+    ASSIGN, EQ, LT, PLUS, MINUS,
+    TIMES, OVER, LPAREN, RPAREN, DOT,
+    COLON, SEMI, COMMA, LMIDPAREN, RMIDPAREN,
+    UNDERANGE
+};
+
 
 
 class Token
 {
 public:
     Token();
+
+private:
+    int lineShow; //记录该单词在源程序中的行数
+    LexType lex; //记录该单词的词法信息
+    QString sem; //记录该单词的语义信息
 };
 
 #endif // TOKEN_H
