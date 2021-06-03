882c18857fae813d37794d42a967b33c17db1a43
ztmail723 <ztmail723@gmail.com>
2021-06-03 09:47:32 +0800

    完善了UI接口

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,88 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include "lexerimp.h"
+#include "parserll1.h"
+#include "parserrecursivedescent.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    tokenList = nullptr;
+    syntaxTree = nullptr;
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    delete tokenList;
+    delete syntaxTree;
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked() // 从文件中读取
+{
+    //定义文件对话框类
+    QFileDialog* fileDialog = new QFileDialog(this);
+    //定义文件对话框标题
+    fileDialog->setWindowTitle(QStringLiteral("选中文件"));
+    //设置默认文件路径
+    // fileDialog->setDirectory(".");
+    //设置文件过滤器
+    fileDialog->setNameFilter(tr("文本文件(*.txt)"));
+    //设置可以选择多个文件,默认为只能选择一个文件QFileDialog::ExistingFiles
+    fileDialog->setFileMode(QFileDialog::ExistingFiles);
+    //设置视图模式
+    fileDialog->setViewMode(QFileDialog::Detail);
+    //打印所有选择的文件的路径
+    QStringList fileNames;
+    if (fileDialog->exec())
+    {
+        fileNames = fileDialog->selectedFiles();
+    }
+    QFile file(fileNames.back());
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+    QByteArray t = file.readAll();
+    ui->textEdit->setText(QString(t));
+    file.close();
+}
+
+void MainWindow::on_pushButton_2_clicked() // 进行词法分析
+{
+    Lexer* lexer = new LexerImp();
+    if (this->tokenList != nullptr)
+    {
+        delete this->tokenList;
+        this->tokenList = nullptr;
+    }
+    this->tokenList = lexer->run(ui->textEdit->toPlainText());
+    delete lexer;
+}
+
+void MainWindow::on_pushButton_3_clicked() // 进行语法分析
+{
+    if (this->tokenList == nullptr)
+    {
+        QMessageBox::about(nullptr, "提示", "尚未进行词法分析，无法进行语法分析");
+        return;
+    }
+    int selectedID = ui->comboBox->currentIndex();
+    Parser* parser = nullptr;
+    if (selectedID == 0)
+    {
+        parser = new ParserRecursiveDescent(); // 递归下降
+    }
+    else
+    {
+        parser = new ParserLL1(); // LL1
+    }
+    if (this->syntaxTree != nullptr)
+    {
+        delete this->syntaxTree;
+        this->syntaxTree = nullptr;
+    }
+    this->syntaxTree = parser->run(*this->tokenList);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+class TokenList;
+class SyntaxTree;
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -12,10 +13,19 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
+    TokenList* tokenList;
+    SyntaxTree* syntaxTree;
 };
 #endif // MAINWINDOW_H
