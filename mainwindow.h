7b0f39a581f7e669d7445e316e69d8fd80d45c59
ztmail723@gmail.com <44718750+ztmail723@users.noreply.github.com>
2021-06-12 23:29:42 +0800

    给析构函数添加了override标注

--- mainwindow.h
+++ mainwindow.h
@@ -16,7 +16,7 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget* parent = nullptr);
-    ~MainWindow();
+    ~MainWindow() override;
 
 private slots:
     void on_pushButton_clicked();
